55935d71313f15eb26f91ba737cedb1c600915d8
Mykola Skrynnyk <45297092+mykolaskrynnyk@users.noreply.github.com>
2026-05-13 15:05:17 +0200

    Refactor(brand): use meta section to configure custom components

--- app.py
+++ app.py
@@ -8,15 +8,16 @@ from shiny import App, render, ui
 
 from src import components, link_undp_css
 
+theme = ui.Theme.from_brand(__file__)
 app_ui = ui.page_fluid(
     ui.head_content(*link_undp_css()),
-    components.header(region="Region", title={"text": "Site Title", "href": "/"}),
+    components.header(**theme.brand.meta.header),
     ui.br(),
     ui.panel_title("Hello Shiny!"),
     ui.input_slider("n", "N", 0, 100, 20),
     ui.output_text_verbatim("txt"),
-    components.footer(),
-    theme=ui.Theme.from_brand(__file__),
+    components.footer(**theme.brand.meta.footer),
+    theme=theme,
 )
 
 
